83229c7c74f2f4439df12b9b045b48114c0f7282
Dafa Ghani <dafagar08@gmail.com>
2026-04-27 01:03:55 +0700

    chore: add ci and prettier formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -10,16 +10,16 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'error',
-    }
-  }
+    },
+  },
 ]);
 
 export default eslintConfig;
