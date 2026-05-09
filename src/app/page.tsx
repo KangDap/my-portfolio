a236381c27709aa0e80261f4ddac8fc9cd2e1408
Dafa Ghani <dafagar08@gmail.com>
2026-05-09 23:46:06 +0700

    feat: update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { ExperiencesTab } from '@/components/experiences-tab';
 import { HeroSection } from '@/components/hero-section';
+import { Navbar } from '@/components/navbar';
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+      <section id="home">
+        <HeroSection />
+      </section>
+      <section id="experiences" className="mx-auto w-full max-w-6xl px-6 pb-20">
+        <ExperiencesTab />
+      </section>
+    </main>
+  );
 }
